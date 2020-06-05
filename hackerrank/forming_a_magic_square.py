9934c6dab77361737d99b8650ed670bb14ce134c
Byung Il Choi <choi@byung.org>
2020-06-05 03:49:05 -0500

    added problem and solution for 'forming a magic square' from HackerRank

---
+++ hackerrank/forming_a_magic_square.py
@@ -0,0 +1,29 @@
+#!/bin/python3
+#
+# Problem Author: pkacprzak @ hackerrank
+#     Difficulty: Easy
+#           link: https://www.hackerrank.com/challenges/magic-square-forming/problem
+#
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the formingMagicSquare function below.
+def formingMagicSquare(A):
+    # your solution
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s = []
+
+    for _ in range(3):
+        s.append(list(map(int, input().rstrip().split())))
+
+    result = formingMagicSquare(s)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
